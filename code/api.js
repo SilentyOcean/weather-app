1ffc4c9594f13710c6e1893e1e36baf277048267
SilentyOcean <hoq6884@gmail.com>
2025-02-25 08:47:49 +0700

    Change from hourly to current weather

--- code/api.js
+++ code/api.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function(){
     async function fetchData(latitude, longitude){
         try{
             
-            const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=precipitation,rain,showers,snowfall&hourly=temperature_2m,relative_humidity_2m,precipitation&timezone=Asia%2FSingapore`);
+            const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,precipitation,rain,showers,snowfall&hourly=temperature_2m&timezone=Asia%2FSingapore`);
             if(!response.ok){
                 throw new Error("Could not fetch error");
             }
@@ -40,6 +40,7 @@ document.addEventListener("DOMContentLoaded", function(){
         let longitude = locationData.longitude;
         let geolocation = locationData.geolocation;
 
+        document.getElementById("city").innerText = geolocation;
        
         document.getElementById("city").innerText = geolocation;
 
@@ -66,41 +67,21 @@ document.addEventListener("DOMContentLoaded", function(){
             return;
         }
 
-        
-        let temperatures = hourly.temperature_2m;
-        let temp_length = temperatures.length;
-        let latest_temp = temperatures[temp_length - 1];
-        console.log('Latest temperature: '+latest_temp);
+       let current = data.current;
+       let current_time = current.time;
+       let current_temp = current.temperature_2m;
+       let current_humid = current.relative_humidity_2m;
+       let current_precipitation = current.precipitation;
+       let current_rain = current.rain;
+       let current_shower = current.showers;
+       let current_snowfall = current.snowfall;
+
 
        
-
-        let time = hourly.time;
-        let time_length = time.length;
-        let latest_time = time[time_length - 1];
-        console.log('Latest time: '+latest_time);
-
-        let precipitation = hourly.precipitation;
-        let latest_precipitation = precipitation[time_length-1];
-        console.log('Latest precipitation: '+latest_precipitation);
-        
-
-    
-        let relative_humidity_2m = hourly.relative_humidity_2m;
-        let latest_humid = relative_humidity_2m[time_length -1];
-
-        //Display temperature
-        let temp_display = document.getElementById("temperature");
-        temp_display.innerText = latest_temp + 'ºC';
-
-
-        //Display Humidity
-        let humid_display = document.getElementById("relative_humidity");
-        humid_display.innerText = `Humidity: ${latest_humid}%`;
-        
-        
-        //Display precipation
-        let precipation_display = document.getElementById("precipation");
-        precipation_display.innerText = `Precipitation ${latest_precipitation}%`;
+       
+       document.getElementById("temperature").innerText = `Temperature: ${current_temp}ºC`;
+       document.getElementById("relative_humidity").innerText = `Humidity: ${current_humid}%`;
+       document.getElementById("precipitation").innerText = `Precipitation: ${current_precipitation}%`;
        
 
 
